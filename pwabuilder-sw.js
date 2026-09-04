4752d8183313e7eb715bc821fb6348867e942dc9
Techno Power Bangladesh <73015576+technopower@users.noreply.github.com>
2026-09-04 23:28:36 +0600

    Create pwabuilder-sw.js

---
+++ pwabuilder-sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'diquiz-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/logos/index_logo.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
